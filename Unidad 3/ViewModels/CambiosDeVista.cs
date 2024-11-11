57f6287cfa647e62b7b18541a69c9fe41a1d8a90
Cristian221G0298 <221G0298@rcarbonifera.tecnm.mx>
2024-11-11 07:37:55 -0600

    Cambios

---
+++ Unidad 3/ViewModels/CambiosDeVista.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Unidad_3.ViewModels
+{
+    public enum Vistas { Menú, ImageBrush, VisualBrush, Arcoíris, Colores, Corbata, CorbataConPuntos, Foco, Ojo, PacMan, Paleta, Sonaja }
+    public class CambiosDeVista : INotifyPropertyChanged
+    {
+        public ICommand CambioVista { get; set; }
+        public Vistas Vista { get; set; }
+        public CambiosDeVista()
+        {
+            CambioVista = new RelayCommand<Vistas>(CambiarVista);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void CambiarVista(Vistas v)
+        {
+            Vista = v;
+            PropertyChanged?.Invoke(this, new(nameof(Vista)));
+        }
+    }
+}
---
+++ Unidad 3/Views/Menú.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Unidad_3.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Menú.xaml
+    /// </summary>
+    public partial class Menú : UserControl
+    {
+        public Menú()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLíneas_Click(object sender, RoutedEventArgs e)
+        {
+            Líneas L = new();
+            L.ShowDialog();
+        }
+
+        private void btnElípses_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 W = new();
+            W.ShowDialog();
+        }
+    }
+}
